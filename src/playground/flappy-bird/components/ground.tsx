f1ff48a61aba72c957d5ba2752f4330766c1c417
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-18 22:25:22 -0500

    Create ground.tsx

---
+++ src/playground/flappy-bird/components/ground.tsx
@@ -0,0 +1,21 @@
+import { Box, styled } from '@mui/material';
+import { brown } from '@mui/material/colors';
+import noise from 'assets/noise.svg';
+
+const Ground = styled(Box)(({ theme }) => ({
+  alignItems: 'flex-start',
+  backgroundColor: brown['300'],
+  backgroundImage: `linear-gradient(
+    to bottom,
+    ${brown['500']},
+    transparent
+  ),
+  url(${noise})`,
+  display: 'flex',
+  filter: 'contrast(100%) brightness(100%)',
+  gridColumn: 1,
+  gridRow: 2,
+  height: '100%',
+}));
+
+export default Ground;
