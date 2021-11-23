57a9c16153a4c1a27fdbeaabc18375f9e3e4580d
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-22 22:27:17 -0500

    Update index.tsx

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,13 +1,17 @@
-import FlappyBird from 'playground/flappy-bird';
+import Landing from 'landing';
+import i18n from 'locales';
 import { StrictMode } from 'react';
+import { I18nextProvider } from 'react-i18next';
 import AppThemeProvider from 'styles/provider';
 
 function Core() {
   return (
     <StrictMode>
-      <AppThemeProvider>
-        <FlappyBird />
-      </AppThemeProvider>
+      <I18nextProvider i18n={i18n}>
+        <AppThemeProvider>
+          <Landing />
+        </AppThemeProvider>
+      </I18nextProvider>
     </StrictMode>
   );
 }
