53dccf1565084be3166d2dbe0947801e1afd336e
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-23 23:46:46 -0500

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,7 @@
 import { IconButton, styled, Tooltip } from '@mui/material';
 import me from 'assets/me.png';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Img = styled('img')(({ theme }) => ({
   width: theme.spacing(4),
@@ -13,8 +14,8 @@ function Logo() {
     <Tooltip title={t('Landing.Logo.Tooltip') as string}>
       <IconButton
         size="small"
-        component="a"
-        href="//whois.kamilo.dev"
+        component={Link}
+        to="/"
       >
         <Img src={me} alt={t('AltText')} />
       </IconButton>
