122b494718219cbc17beb379138485f4e0019803
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-23 23:42:46 -0500

    Update index.tsx

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,15 +1,15 @@
-import Landing from 'landing';
 import i18n from 'locales';
 import { StrictMode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import AppThemeProvider from 'styles/provider';
+import Router from './router';
 
 function Core() {
   return (
     <StrictMode>
       <I18nextProvider i18n={i18n}>
         <AppThemeProvider>
-          <Landing />
+          <Router />
         </AppThemeProvider>
       </I18nextProvider>
     </StrictMode>
