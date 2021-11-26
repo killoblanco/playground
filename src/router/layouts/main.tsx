ab2e8d598006bb14e775d83ab8d6ea6d8cea5989
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-25 21:37:15 -0500

    Update main.tsx

--- src/router/layouts/main.tsx
+++ src/router/layouts/main.tsx
@@ -1,12 +1,28 @@
 import { Container } from '@mui/material';
-import { PropsWithChildren } from 'react';
 import Footer from 'components/footer';
 import NavBar from 'components/nav-bar';
 import Section from 'components/section';
+import { PropsWithChildren } from 'react';
+import SEO from '../../components/seo';
 
-function MainLayout({children}: PropsWithChildren<any>) {
+function MainLayout({ children }: PropsWithChildren<any>) {
   return (
     <Section>
+      <SEO
+        title="Playground"
+        description="Take a look at my playground where I show you fun projects that I have done in my free time"
+        keywords={[
+          'playground',
+          'react',
+          'typescript',
+          'javascript',
+          'frontend',
+          'web development',
+          'web design',
+          'design',
+          'kamilo vasquez'
+        ]}
+      />
       <Container fixed>
         <NavBar />
         {children}
